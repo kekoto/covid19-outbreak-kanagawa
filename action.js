906ecd254b1ed111fd3ac04ca3ee7fe1975bba0b
kekoto <oozekima+kekoto4@gmail.com>
2020-05-05 15:31:13 +0900

    update

--- action.js
+++ action.js
@@ -2,12 +2,6 @@ const axios = require('axios');
 const qs = require('querystring');
 
 const LINE_NOTIFY_API_URL = 'https://notify-api.line.me/api/notify';
-
-// GitHub Actions で実行する際に Secret 値 LINE_TOKEN を利用する
-// 
-// 実際には、GitHub Actions の
-// run: LINE_TOKEN=${{secrets.LINE_TOKEN}} node action.js
-// という書き方で渡されています
 const LINE_NOTIFY_TOKEN = process.env.LINE_TOKEN;
 
 let config = {
@@ -18,22 +12,12 @@ let config = {
         'Authorization': 'Bearer ' + LINE_NOTIFY_TOKEN
     },
     data: qs.stringify({
-        message: 'action.jsからの通知だよ。',
+        message: 'ProtoOut Studioからの通知だよー！',
     })
 }
 
-async function getRequest() {
-
-  ////// LINE Notify に送る ////////////////////////
-
-  try {
-    const responseLINENotify = await axios.request(config);
-    console.log(responseLINENotify.data);
-  } catch (error) {
-    console.error(error);
-  }
-
+module.exports = async function(postData){
+    config.data = qs.stringify(postData)
+    const response = await axios.request(config);
+    console.log(response.data);
 }
-
-// getRequest を呼び出してデータを読み込む
-getRequest();
--- kanagawa.js
+++ kanagawa.js
@@ -3,15 +3,6 @@
 const axios = require("axios");
 const fs = require("fs"); //注: npm i 不要
 
-const linenotify = require('./action.js');
-/*
-const message = 'hogehoge'
-linenotify({
-  message: message,
-})
-*/
-
-
 //データ更新関数
 async function updateData(newData) {
   const PATH = "./docs/data.json";
@@ -23,21 +14,44 @@ async function getRequest() {
   let response;
   try {
     response = await axios.get(
-      "https://transit.yahoo.co.jp/traininfo/detail/21/0/"
+      "https://www.pref.kanagawa.jp/docs/ga4/bukanshi/occurrence.html"
     );
     // console.log(response.data);
     let html = response.data;
     html = html.replace(/\r?\n/g, ""); //整形1: 改行などを削除して整形しやすくする
+    //console.log(html);
 
     //時間
-    let jikan = html.match(/class="subText">(.*?)<\/span>/)[1];
-    jikan = jikan.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, ""); //整形2: タグを削除
+    //let jikan = html.match(/class="subText">(.*?)<\/span>/)[1];
+    //jikan = jikan.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, ""); //整形2: タグを削除
     //console.log(jikan);
 
-    //運行情報
-    let unko = html.match(/id="mdServiceStatus">(.*?)<\/div>/)[1];
-    unko = unko.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, ""); //整形2: タグを削除
+    //市町村情報
+    let unko = html.match(/居住地別累計(.*?)<\/tbody>/)[1];
+    let shichoson = unko.match(/bg_blue">(.*?)<\/td>/g);
+    let kazu = unko.match(/enter;">(.*?)<\/td>/g);
+    shichoson = shichoson.map(item => item.match(/bg_blue">(.*?)<\/td>/)[1])
+    kazu = kazu.map(item => item.match(/enter;">(.*?)<\/td>/)[1])
+    shichoson.splice(0, 1);
+    shichoson.splice(7, 1);
+    //console.log(shichoson, kazu);
+    const saveData = []
+    //for (let i = 0,len = shichoson.length; i < len; i++) {
+    //saveData[i] = {
+    //    shichoson: shichoson[i],
+    //    ruikei: kazu[i]
+    //}
+    for (let i = 0,len = shichoson.length; i < len; i++) {
+      saveData[i] = {
+        [shichoson[i]]: kazu[i]
+      }
+    }
+    console.log(saveData)
+    //unko = unko.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, ""); //整形2: タグを削除
     //console.log(unko);
+
+    return;
+
 
     // 前回のデータ
     const PATH = "./docs/data.json";
@@ -51,19 +65,19 @@ async function getRequest() {
     // 最新の情報に書き換え
     yamanoteData.new.date = jikan;
     yamanoteData.new.msg = unko;
-
+ 
     // 変更されているか確認
     if(　yamanoteData.new.date != yamanoteData.old.date ){
       console.log(yamanoteData);
       // 変更があれば更新
       const saveData = yamanoteData;
       await updateData(saveData); //データ更新関数を実行
-
+ 
       const message = 'yamanote.jsから'
       linenotify({
         message: message,
       })
-
+ 
     }
     else {
       console.log('変更なし');
@@ -72,11 +86,11 @@ async function getRequest() {
         message: message,
       })
 
-    }
-  }catch (error) {
-    console.error(error);
-  }
-}
-
-// getRequest を呼び出してデータを読み込む
-getRequest();
+     }
+   }catch (error) {
+     console.error(error);
+   }
+ }
+ 
+ // getRequest を呼び出してデータを読み込む
+ getRequest();
