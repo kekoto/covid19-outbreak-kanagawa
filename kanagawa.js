3accabf1f986b72b7137d67a1e32c05cad26bae2
kekoto <oozekima+kekoto4@gmail.com>
2020-05-07 23:41:57 +0900

    update

--- kanagawa.js
+++ kanagawa.js
@@ -26,7 +26,7 @@ async function getRequest() {
     //市町村情報
     let ruikei = html.match(/居住地別累計(.*?)<\/tbody>/)[1];
     let shichoson = ruikei.match(/bg_blue">(.*?)<\/td>/g);
-    let kazu = unko.match(/enter;">(.*?)<\/td>/g);
+    let kazu = ruikei.match(/enter;">(.*?)<\/td>/g);
     shichoson = shichoson.map(item => item.match(/bg_blue">(.*?)<\/td>/)[1])
     kazu = kazu.map(item => item.match(/enter;">(.*?)<\/td>/)[1])
     shichoson.splice(0, 1);
